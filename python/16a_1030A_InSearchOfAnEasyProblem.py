c3a4a079c9a91f60cc1f83b598af5fc5a503af00
Anish Felix Mathias <115111096+anishfelixm@users.noreply.github.com>
2023-12-20 18:06:07 +0530

    Add files via upload

---
+++ python/16a_1030A_InSearchOfAnEasyProblem.py
@@ -0,0 +1,10 @@
+def main():
+    n = int(input())
+    replies = input().split()
+    if '1' in replies:
+        print("HARD")
+    else:
+        print("EASY")
+
+if __name__ == "__main__":
+    main()
