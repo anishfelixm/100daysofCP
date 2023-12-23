9d535ffe8738107be08f2085836da58e8e4f6f57
Anish Felix Mathias <115111096+anishfelixm@users.noreply.github.com>
2023-12-23 10:59:35 +0530

    Add files via upload

---
+++ python/20b_148A_InsomniaCure.py
@@ -0,0 +1,14 @@
+def main():
+    k = int(input())
+    l = int(input())
+    m = int(input())
+    n = int(input())
+    d = int(input())
+    cnt = 0
+    for i in range(1,d+1):
+        if i % k == 0 or i % l == 0 or i % m == 0 or i % n == 0:
+            cnt += 1
+    print(cnt)
+
+if __name__ == "__main__":
+    main()
