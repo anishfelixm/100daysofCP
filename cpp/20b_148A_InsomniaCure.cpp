4d27fe93a68b189f91320847bb49071fdb06cf00
Anish Felix Mathias <115111096+anishfelixm@users.noreply.github.com>
2023-12-23 10:59:15 +0530

    Add files via upload

---
+++ cpp/20b_148A_InsomniaCure.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+int main(){
+    int k, l, m, n, d, cnt = 0;
+    std::cin>>k>>l>>m>>n>>d;
+    for(int i = 1; i <= d; i++){
+        if(i % k != 0 && i % l != 0 && i % m != 0 && i % n != 0){
+            cnt++;
+        }
+    }
+    std::cout<<d-cnt;
+    return 0;
+}
