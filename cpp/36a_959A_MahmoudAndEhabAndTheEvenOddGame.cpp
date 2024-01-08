4efe5dd438f1a2f99feeac6478eff54d1f251398
Anish Felix Mathias <115111096+anishfelixm@users.noreply.github.com>
2024-01-08 08:26:05 +0530

    Add files via upload

---
+++ cpp/36a_959A_MahmoudAndEhabAndTheEvenOddGame.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+int main(){
+    int n;
+    std::cin>>n;
+    if(n % 2 == 0){
+        std::cout<<"Mahmoud";
+    }
+    else{
+        std::cout<<"Ehab";
+    }
+    return 0;
+}
