ef4b86b1020528bc2969160a52a2e6dac5d3d996
Anish Felix Mathias <115111096+anishfelixm@users.noreply.github.com>
2024-02-03 01:47:14 +0530

    Add files via upload

---
+++ cpp/61b_1526A_MeanInequality.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solve(vector<int> arr, int n){
+    vector<int> ans;
+    sort(arr.begin(), arr.end());
+    for(int i = 0; i < n; i++){
+        ans.push_back(arr[(2*n)-1-i]);
+        ans.push_back(arr[i]);
+    }
+    return ans;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t, n, temp;
+    vector<vector<int>>ans;
+    cin>>t;
+
+    for(int i = 0; i < t; i++){
+        cin>>n;
+        vector<int> arr;
+        for(int j = 0; j < (2 *n); j++){
+            cin>>temp;
+            arr.push_back(temp);
+        }
+        ans.push_back(solve(arr, n));
+    }
+    
+    for(auto i : ans){
+        for(auto j : i){
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
