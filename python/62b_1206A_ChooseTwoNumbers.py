8d30f943bf0014424796c174b3f3cd46c4bc2c92
Anish Felix Mathias <115111096+anishfelixm@users.noreply.github.com>
2024-02-04 13:29:28 +0530

    Add files via upload

---
+++ python/62b_1206A_ChooseTwoNumbers.py
@@ -0,0 +1,9 @@
+def main():
+    n = int(input())
+    a = list(map(int, input().split()))
+    m = int(input())
+    b = list(map(int, input().split()))
+    print(max(a), max(b))
+
+if __name__ == "__main__":
+    main()
